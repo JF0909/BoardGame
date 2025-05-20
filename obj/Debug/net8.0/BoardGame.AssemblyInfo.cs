39e118a53159ac89daa2e22f91cb61c3f4fc6c86
Jingwen Fu <n11886609@qut.edu.au>
2025-05-20 16:26:58 +1000

    create NotaktoCommand

--- obj/Debug/net8.0/BoardGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/BoardGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoardGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f627c256040d1d2deca250d5345cb2b3ed1e7643")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30f4408abb439b09d041fa13f9a5b5f1887af27c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoardGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoardGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
